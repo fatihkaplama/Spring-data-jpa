b3ebe9aa4ef3e4581a29a04e816e99f5a2119102
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-01-02 20:45:31 +0300

    Add table annotation

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import javax.persistence.*;
 
 @Entity(name = "Student")
+@Table(
+        name = "Student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        }
+)
 public class Student {
 
     @Id
@@ -35,9 +41,7 @@ public class Student {
     @Column(
             name = "email",
             nullable = false,
-            columnDefinition = "TEXT",
-            unique = true
-
+            columnDefinition = "TEXT"
     )
     private String email;
     @Column(
@@ -59,7 +63,7 @@ public class Student {
     }
 
     public Student() {
-        
+
     }
 
     public Long getId() {
