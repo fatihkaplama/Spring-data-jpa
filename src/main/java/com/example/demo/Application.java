e01615a259659d9dd97fe1118e9e7b6b0e0ffcfa
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-01-02 21:05:45 +0300

    Add student repository

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student fatih = new Student(
+                    "Fatih",
+                    "Kaplama",
+                    "fatih@gmail.com",
+                    22);
+            studentRepository.save(fatih);
+        };
     }
 
 }
